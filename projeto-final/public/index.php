f54008ad7d24715682eeefe00dd986a8833a1959
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-13 15:09:34 -0300

    feat: move routes for config/routes

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', 1);
 include '../vendor/autoload.php';
 
+// instanciando direto no index principal -------------------------------------------------
+
 // $database='mysql';
 // $username='root';
 // $password='12345';
@@ -11,54 +13,39 @@ include '../vendor/autoload.php';
 // // string de concção
 // $connection = new PDO('mysql:host=localhost;dbname='.$database,$username,$password);
 
-use App\Connection\Connection;
 
-$connection = Connection::getConnection();
+// ----------- recebendo a conecção feita no connectio -------------------------
+// use App\Connection\Connection;
 
-// query no banco
-$query = 'SELECT * FROM tb_category';
-// recebendo a query no antes de executar
-$preparacao = $connection->prepare($query);
-// executando o nosso codigo
-$preparacao->execute();
+// $connection = Connection::getConnection();
 
-// fazendo uma chamada fetch e recebendo os dados do banco
-var_dump($preparacao->fetch()); 
+// // query no banco
+// $query = 'SELECT * FROM tb_category';
+// // recebendo a query no antes de executar
+// $preparacao = $connection->prepare($query);
+// // executando o nosso codigo
+// $preparacao->execute();
 
-while($registro = $preparacao->fetch()) {
-  var_dump($registro);
+// // fazendo uma chamada fetch e recebendo os dados do banco
+// var_dump($preparacao->fetch()); 
+
+// while($registro = $preparacao->fetch()) {
+//   var_dump($registro);
+// }
+
+use App\Controller\ErrorController;
+
+
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+
+$routes = include '../src/config/routes.php';
+
+if(false === isset($routes[$url])){
+    (new ErrorController()) ->notFoundAction();
+    exit;
 }
-// use App\Controller\IndexController;
-// use App\Controller\ProductController;
-// use App\controller\CategoryController;
-// use App\Controller\ErrorController;
 
+$controllerName = $routes[$url]['controller'];
+$methodName = $routes[$url]['method'];
 
-// $url = explode('?', $_SERVER['REQUEST_URI'])[0];
-
-// function CreateRoute(string $controllerName, string $methodName){
-//     return [
-//         'controller' => $controllerName,
-//         'method' => $methodName,
-//     ];
-// }
-
-// $routes=[
-//     '/' => CreateRoute(IndexController::class, 'indexAction'),
-//     '/produtos' => CreateRoute(ProductController::class, 'listAction'),
-//     '/produtos/novo' => CreateRoute(ProductController::class, 'addAction'),
-//     '/produtos/editar' => CreateRoute(ProductController::class, 'editAction'),
-//     '/categoria' => CreateRoute(CategoryController::class, 'listAction'),
-//     '/categoria/novo' => CreateRoute(CategoryController::class, 'addAction'),
-//     '/categoria/edit' => CreateRoute(CategoryController::class, 'editAction'),
-// ];
-
-// if(false === isset($routes[$url])){
-//     (new ErrorController()) ->notFoundAction();
-//     exit;
-// }
-
-// $controllerName = $routes[$url]['controller'];
-// $methodName = $routes[$url]['method'];
-
-// (new $controllerName()) -> $methodName();
+(new $controllerName()) -> $methodName();
---
+++ projeto-final/src/config/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Controller\IndexController;
+use App\Controller\ProductController;
+use App\controller\CategoryController;
+function CreateRoute(string $controllerName, string $methodName){
+    return [
+        'controller' => $controllerName,
+        'method' => $methodName,
+    ];
+}
+
+$routes=[
+  '/' => CreateRoute(IndexController::class, 'indexAction'),
+  '/produtos' => CreateRoute(ProductController::class, 'listAction'),
+  '/produtos/novo' => CreateRoute(ProductController::class, 'addAction'),
+  '/produtos/editar' => CreateRoute(ProductController::class, 'editAction'),
+  '/categoria' => CreateRoute(CategoryController::class, 'listAction'),
+  '/categoria/novo' => CreateRoute(CategoryController::class, 'addAction'),
+  '/categoria/edit' => CreateRoute(CategoryController::class, 'editAction'),
+];
+
+return $routes;
